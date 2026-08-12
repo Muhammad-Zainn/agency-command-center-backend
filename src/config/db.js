5081f45dc08c37e97ee2988280b5c712034ccdbc
Muhammad-Zainn <zainjamil84@gmail.com>
2026-08-12 21:02:43 +0500

    Add Docker configuration and update DB connection

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`Connected to MongoDB Atlas: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Database connection failed: ${error.message}`);
